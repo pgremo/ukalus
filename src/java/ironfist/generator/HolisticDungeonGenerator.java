cfe74b9af061caf05c74fb95967dcec56397f615
gremopm <gremopm@f2a0fb1d-5e3f-4af0-b388-fe59793aa704>
2005-01-20 22:15:50 +0000

    code clean up
    git-svn-id: svn://localhost/trunk@508 f2a0fb1d-5e3f-4af0-b388-fe59793aa704

--- src/java/ironfist/generator/HolisticDungeonGenerator.java
+++ src/java/ironfist/generator/HolisticDungeonGenerator.java
@@ -11,13 +11,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-
 /**
  * DOCUMENT ME!
- *
+ * 
  * @author pmgremo
  */
 public class HolisticDungeonGenerator {
+
   private Random randomizer = new Random();
   private double room_probability = .8;
   private int min_room_height = 4;
@@ -48,39 +48,39 @@ public class HolisticDungeonGenerator {
 
     for (int index = 0; (index < cuts) && (valid.size() > 0); index++) {
       Region current = (Region) valid.remove((valid.size() > 1)
-          ? randomizer.nextInt(valid.size() - 1) : 0);
+          ? randomizer.nextInt(valid.size() - 1)
+          : 0);
       Region next = null;
       boolean splitWidth = current.getWidth() > (min_region_width * 2);
       boolean splitHeight = current.getHeight() > (min_region_height * 2);
 
-      if ((splitHeight && !splitWidth) ||
-          (splitWidth && splitHeight && direction)) {
-        int height = min_region_height +
-          randomizer.nextInt(current.getHeight() - (min_region_height * 2));
+      if ((splitHeight && !splitWidth)
+          || (splitWidth && splitHeight && direction)) {
+        int height = min_region_height
+            + randomizer.nextInt(current.getHeight() - (min_region_height * 2));
         current.setHeight(current.getHeight() - height);
-        next = new Region(current.getX(),
-            current.getY() + current.getHeight() + 1, current.getWidth(),
-            height - 1);
+        next = new Region(current.getX(), current.getY() + current.getHeight()
+            + 1, current.getWidth(), height - 1);
       } else if (splitWidth) {
-        int width = min_region_width +
-          randomizer.nextInt(current.getWidth() - (min_region_width * 2));
+        int width = min_region_width
+            + randomizer.nextInt(current.getWidth() - (min_region_width * 2));
         current.setWidth(current.getWidth() - width);
         next = new Region(current.getX() + current.getWidth() + 1,
-            current.getY(), width - 1, current.getHeight());
+          current.getY(), width - 1, current.getHeight());
       }
 
       direction = !direction;
 
-      if ((current.getWidth() > (min_region_width * 2)) ||
-          (current.getHeight() > (min_region_height * 2))) {
+      if ((current.getWidth() > (min_region_width * 2))
+          || (current.getHeight() > (min_region_height * 2))) {
         valid.add(current);
       } else {
         result.add(current);
       }
 
       if (next != null) {
-        if ((next.getWidth() > (min_region_width * 2)) ||
-            (next.getHeight() > (min_region_height * 2))) {
+        if ((next.getWidth() > (min_region_width * 2))
+            || (next.getHeight() > (min_region_height * 2))) {
           valid.add(next);
         } else {
           result.add(next);
@@ -93,64 +93,33 @@ public class HolisticDungeonGenerator {
     return result;
   }
 
-  //  private void drawDungeon(List cells) {
-  //    Iterator iterator = cells.iterator();
-  //
-  //    while (iterator.hasNext()) {
-  //      Region cell = (Region) iterator.next();
-  //
-  //      if (cell.room != null) {
-  //        cell = cell.room;
-  //
-  //        for (int x = cell.getX(); x <= (cell.getWidth() + cell.getX()); x++) {
-  //          level[cell.getY()][x] = true;
-  //          level[cell.getY() + cell.getHeight()][x] = true;
-  //        }
-  //
-  //        for (int y = cell.getY(); y <= (cell.getHeight() + cell.getY()); y++) {
-  //          level[y][cell.getX()] = true;
-  //          level[y][cell.getX() + cell.getWidth()] = true;
-  //        }
-  //      }
-  //    }
-  //  }
-  //
-  //  private void drawDungeon(int[][] data) {
-  //    for (int y = 0; y < data.length; y++) {
-  //      for (int x = 0; x < data[y].length; x++) {
-  //        if ((data[y][x] == 2) || (data[y][x] == 4)) {
-  //          level[y][x] = true;
-  //        }
-  //      }
-  //    }
-  //  }
   private void createRooms(List regions) {
     Collections.shuffle(regions, randomizer);
 
     int index = 0;
 
-    for (; index < (((double) regions.size()) * room_probability); index++) {
+    for (; index < (regions.size() * room_probability); index++) {
       Region current = (Region) regions.get(index);
 
       Region room = new Region(current);
 
       if (current.getWidth() > min_room_width) {
-        room.setWidth(min_room_width +
-          randomizer.nextInt(current.getWidth() - min_room_width));
+        room.setWidth(min_room_width
+            + randomizer.nextInt(current.getWidth() - min_room_width));
 
         if (current.getWidth() > room.getWidth()) {
-          room.setX(current.getX() +
-            randomizer.nextInt(current.getWidth() - room.getWidth()));
+          room.setX(current.getX()
+              + randomizer.nextInt(current.getWidth() - room.getWidth()));
         }
       }
 
       if (current.getHeight() > min_room_height) {
-        room.setHeight(min_room_height +
-          randomizer.nextInt(current.getHeight() - min_room_height));
+        room.setHeight(min_room_height
+            + randomizer.nextInt(current.getHeight() - min_room_height));
 
         if (current.getHeight() > room.getHeight()) {
-          room.setY(current.getY() +
-            randomizer.nextInt(current.getHeight() - room.getHeight()));
+          room.setY(current.getY()
+              + randomizer.nextInt(current.getHeight() - room.getHeight()));
         }
       }
 
@@ -172,7 +141,6 @@ public class HolisticDungeonGenerator {
     int floor = 1;
     int wall = 2;
     int corner = 4;
-    int connected = 8;
     Iterator iterator = regions.iterator();
 
     while (iterator.hasNext()) {
@@ -183,13 +151,13 @@ public class HolisticDungeonGenerator {
 
         for (int y = 0; y < cell.getHeight(); y++) {
           for (int x = 0; x < cell.getWidth(); x++) {
-            if (((y == 0) && (x == 0)) ||
-                ((y == (cell.getHeight() - 1)) && (x == 0)) ||
-                ((y == 0) && (x == (cell.getWidth() - 1))) ||
-                ((y == (cell.getHeight() - 1)) && (x == (cell.getWidth() - 1)))) {
+            if (((y == 0) && (x == 0))
+                || ((y == (cell.getHeight() - 1)) && (x == 0))
+                || ((y == 0) && (x == (cell.getWidth() - 1)))
+                || ((y == (cell.getHeight() - 1)) && (x == (cell.getWidth() - 1)))) {
               level[y + cell.getY()][x + cell.getX()] = corner;
-            } else if ((y == 0) || (x == 0) || (y == (cell.getHeight() - 1)) ||
-                (x == (cell.getWidth() - 1))) {
+            } else if ((y == 0) || (x == 0) || (y == (cell.getHeight() - 1))
+                || (x == (cell.getWidth() - 1))) {
               level[y + cell.getY()][x + cell.getX()] = wall;
             } else {
               level[y + cell.getY()][x + cell.getX()] = floor;
@@ -201,22 +169,11 @@ public class HolisticDungeonGenerator {
 
     Collections.shuffle(regions, randomizer);
 
-    Region current = (Region) regions.get(1);
-
-    for (int index = 0; index < regions.size(); index++) {
-      Region next = (Region) regions.get(index);
-
-      if (next.room != null) {
-      }
-    }
-
-    //    drawDungeon(map);
   }
 
   public static void main(String[] args) {
     HolisticDungeonGenerator generator = new HolisticDungeonGenerator();
 
-    //generator.setRandom(new MersenneTwister());
     int[][] result = generator.generate();
 
     for (int x = 0; x < result.length; x++) {
@@ -237,6 +194,7 @@ public class HolisticDungeonGenerator {
   }
 
   private class Region {
+
     Region room;
     private int x;
     private int y;
@@ -289,12 +247,14 @@ public class HolisticDungeonGenerator {
       return y;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see java.lang.Object#toString()
      */
     public String toString() {
-      return "x=" + getX() + ",y=" + getY() + ",width=" + getWidth() +
-      ",height=" + getHeight();
+      return "x=" + getX() + ",y=" + getY() + ",width=" + getWidth()
+          + ",height=" + getHeight();
     }
   }
 }
