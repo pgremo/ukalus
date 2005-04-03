bee7bf06e5a7f6fce2562597b70d9cbb9632e112
pmgremo <pmgremo@f2a0fb1d-5e3f-4af0-b388-fe59793aa704>
2005-04-03 01:46:21 +0000

    removed value field
    git-svn-id: svn://localhost/trunk@586 f2a0fb1d-5e3f-4af0-b388-fe59793aa704

--- src/java/ironfist/container/transformers/BooleanTransformer.java
+++ src/java/ironfist/container/transformers/BooleanTransformer.java
@@ -13,7 +13,6 @@ import ironfist.util.Closure;
 public class BooleanTransformer implements Closure<String, Boolean> {
 
   private static final long serialVersionUID = 3688509878813667384L;
-  private Object value;
 
   public Boolean apply(String value) {
     return Boolean.valueOf(value);
