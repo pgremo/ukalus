f2955b01e25fcbaac6fe812baf318f25d79cb8fe
pmgremo <pmgremo@f2a0fb1d-5e3f-4af0-b388-fe59793aa704>
2005-04-03 01:47:35 +0000

    cleaned up imports
    git-svn-id: svn://localhost/trunk@588 f2a0fb1d-5e3f-4af0-b388-fe59793aa704

--- src/java/ironfist/container/PropertySetter.java
+++ src/java/ironfist/container/PropertySetter.java
@@ -2,7 +2,6 @@ package ironfist.container;
 
 import ironfist.util.Loop;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,13 +14,12 @@ public class PropertySetter {
     this.properties = properties;
   }
 
-  public void setProperties(Object input) throws IllegalArgumentException,
-      IllegalAccessException, InvocationTargetException, InstantiationException {
+  public void setProperties(Object input) throws IllegalArgumentException {
     Method[] methods = input.getClass()
       .getMethods();
     Map<String, Method> setters = new HashMap<String, Method>(methods.length);
     new Loop<Method>(methods).forEach(new SetterCollector(setters));
     new Loop<Map.Entry<String, Resolver>>(properties.entrySet()).forEach(new SetProperty(
-      setters, input));
+        setters, input));
   }
 }
