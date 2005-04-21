7b8096a4cd209d92ddeb77dea3ebb4ac7bfd0d1b
gremopm <gremopm@f2a0fb1d-5e3f-4af0-b388-fe59793aa704>
2005-04-21 17:09:02 +0000

    cleanup
    git-svn-id: svn://localhost/trunk@615 f2a0fb1d-5e3f-4af0-b388-fe59793aa704

--- src/spike/ConvertLists.java
+++ src/spike/ConvertLists.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -6,9 +5,6 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StreamTokenizer;
-import java.util.Iterator;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 /*
  * Created on Feb 24, 2005
@@ -17,7 +13,7 @@ import java.util.TreeSet;
 
 /**
  * @author gremopm
- *  
+ * 
  */
 public final class ConvertLists {
 
@@ -32,26 +28,21 @@ public final class ConvertLists {
     File[] files = inDir.listFiles();
     for (int i = 0; i < files.length; i++) {
       if (files[i].isFile()) {
-        SortedSet<String> words = new TreeSet<String>();
         Reader in = null;
         PrintWriter out = null;
         try {
           in = new BufferedReader(new FileReader(files[i]));
           StreamTokenizer tokenizer = new StreamTokenizer(in);
+          out = new PrintWriter(new FileWriter(new File(outDir,
+            files[i].getName())));
 
           while (tokenizer.nextToken() != StreamTokenizer.TT_EOF) {
             if (tokenizer.ttype == StreamTokenizer.TT_WORD
                 && tokenizer.sval.length() > 1) {
-              words.add(tokenizer.sval.toLowerCase());
+              out.println(tokenizer.sval.toLowerCase());
             }
           }
 
-          out = new PrintWriter(new FileWriter(new File(outDir,
-            files[i].getName())));
-          Iterator iterator = words.iterator();
-          while (iterator.hasNext()) {
-            out.println((String) iterator.next());
-          }
         } finally {
           if (in != null) {
             in.close();
