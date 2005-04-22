84b92ef5bf37e981ab915ab2587fd1f4a6ec1aca
gremopm <gremopm@f2a0fb1d-5e3f-4af0-b388-fe59793aa704>
2005-04-22 18:56:01 +0000

    added more tests
    git-svn-id: svn://localhost/trunk@625 f2a0fb1d-5e3f-4af0-b388-fe59793aa704

--- src/test/ironfist/math/Vector2DTest.java
+++ src/test/ironfist/math/Vector2DTest.java
@@ -1,5 +1,8 @@
 package ironfist.math;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.TestCase;
 
 public class Vector2DTest extends TestCase {
@@ -14,6 +17,26 @@ public class Vector2DTest extends TestCase {
       .rotate(direction));
     assertEquals(Vector2D.get(-3, 0), Vector2D.get(0, -3)
       .rotate(direction));
+  }
+
+  public void testFindInHashMap() {
+    Map<Vector2D, Object> map = new HashMap<Vector2D, Object>();
+    map.put(Vector2D.get(1, 0), null);
+    assertTrue(map.containsKey(Vector2D.get(1, 0)));
+  }
+
+  public void testAdd() {
+    Vector2D result = Vector2D.get(1, 0);
+    Vector2D angle = Vector2D.get(1, 1);
+    result = result.add(angle);
+    assertEquals(Vector2D.get(2, 1), result);
+  }
+
+  public void testSubtract() {
+    Vector2D result = Vector2D.get(1, 0);
+    Vector2D angle = Vector2D.get(1, 1);
+    result = result.subtract(angle);
+    assertEquals(Vector2D.get(0, -1), result);
   }
 
 }
