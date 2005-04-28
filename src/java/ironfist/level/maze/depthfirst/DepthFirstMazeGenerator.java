deebff4708eca2f5760da1b5aefe962cdc69cf41
gremopm <gremopm@f2a0fb1d-5e3f-4af0-b388-fe59793aa704>
2005-04-28 19:55:47 +0000

    refactoring
    git-svn-id: svn://localhost/trunk@637 f2a0fb1d-5e3f-4af0-b388-fe59793aa704

--- src/java/ironfist/level/maze/depthfirst/DepthFirstMazeGenerator.java
+++ src/java/ironfist/level/maze/depthfirst/DepthFirstMazeGenerator.java
@@ -3,106 +3,101 @@ package ironfist.level.maze.depthfirst;
 import ironfist.math.Vector2D;
 import ironfist.util.MersenneTwister;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.Stack;
 
 public class DepthFirstMazeGenerator {
 
-  private static final int EVEN_MASK = Integer.MAX_VALUE - 1;
-  private static final List<Vector2D> DIRECTIONS = Arrays.asList(new Vector2D[]{
+  private static final Vector2D[] DIRECTIONS = new Vector2D[]{
       Vector2D.get(0, 2),
       Vector2D.get(2, 0),
       Vector2D.get(0, -2),
-      Vector2D.get(-2, 0)});
+      Vector2D.get(-2, 0)};
   private Random random;
   private int height;
   private int width;
-  private boolean[][] walls;
+  private boolean[][] cells;
+  private Set<Vector2D> visited = new HashSet<Vector2D>();
+  private Set<Vector2D> all = new HashSet<Vector2D>();
+
+  public DepthFirstMazeGenerator(Random random, int height, int width) {
+    this.random = random;
+    this.height = ((height - 1) & (Integer.MAX_VALUE - 1)) + 1;
+    this.width = ((width - 1) & (Integer.MAX_VALUE - 1)) + 1;
+  }
 
   public boolean[][] generate() {
-    walls = new boolean[height][width];
-    Stack<Vector2D> steps = new Stack<Vector2D>();
-    Vector2D current = Vector2D.get((random.nextInt((height - 1) / 2) * 2) + 1,
-      (random.nextInt((width - 1) / 2) * 2) + 1);
-    set(current, true);
-    while (current != null) {
-      List<Vector2D> directions = getDirections(current);
-      if (!directions.isEmpty()) {
-        Collections.shuffle(directions, random);
-        Vector2D direction = directions.get(0);
-        Vector2D target = current.add(direction);
-        set(target, true);
-        set(current.add(direction.normal()), true);
-        steps.push(current);
-        current = target;
-      } else {
-        current = steps.isEmpty() ? null : steps.pop();
+    cells = new boolean[height][width];
+
+    for (int x = 1; x < cells.length - 1; x++) {
+      for (int y = 1; y < cells[x].length - 1; y++) {
+        if (x % 2 == 1 && y % 2 == 1) {
+          cells[x][y] = true;
+          all.add(Vector2D.get(x, y));
+        }
       }
     }
 
-    return walls;
+    Stack<Vector2D> path = new Stack<Vector2D>();
+    Vector2D current = Vector2D.get((random.nextInt((height - 1) / 2) * 2) + 1,
+      (random.nextInt((width - 1) / 2) * 2) + 1);
+    visited.add(current);
+    while (current != null) {
+      List<Vector2D> neighbors = getNeighbors(current);
+      if (!neighbors.isEmpty()) {
+        Collections.shuffle(neighbors, random);
+        Vector2D neighbor = neighbors.get(0);
+        Vector2D wall = current.add(neighbor.subtract(current)
+          .normal());
+        cells[wall.getX()][wall.getY()] = true;
+        path.push(current);
+        current = neighbor;
+        visited.add(current);
+      } else {
+        current = path.isEmpty() ? null : path.pop();
+      }
+    }
+
+    return cells;
   }
 
-  public List<Vector2D> getDirections(Vector2D location) {
-    List<Vector2D> result = new ArrayList<Vector2D>(4);
+  private List<Vector2D> getNeighbors(Vector2D location) {
+    List<Vector2D> result = new ArrayList<Vector2D>(DIRECTIONS.length);
     for (Vector2D direction : DIRECTIONS) {
-      Vector2D child = location.add(direction);
-      if (contains(child) && !get(child)) {
-        result.add(direction);
+      Vector2D neighbor = location.add(direction);
+      if (!visited.contains(neighbor) && all.contains(neighbor)) {
+        result.add(neighbor);
       }
     }
     return result;
   }
 
-  private void set(Vector2D location, boolean value) {
-    walls[location.getX()][location.getY()] = value;
-
-  }
-
-  private boolean get(Vector2D location) {
-    return walls[location.getX()][location.getY()];
-  }
-
-  private boolean contains(Vector2D location) {
-    return (location.getX() > 0) && (location.getX() < height - 1)
-        && (location.getY() > 0) && (location.getY() < width - 1);
-  }
-
-  public void setRandom(Random random) {
-    this.random = random;
-  }
-
-  public void setHeight(int height) {
-    this.height = ((height - 1) & EVEN_MASK) + 1;
-  }
-
-  public void setWidth(int width) {
-    this.width = ((width - 1) & EVEN_MASK) + 1;
-  }
-
-  public static void main(String[] args) {
-    DepthFirstMazeGenerator generator = new DepthFirstMazeGenerator();
-    generator.setRandom(new MersenneTwister());
-    generator.setHeight(20);
-    generator.setWidth(80);
-
-    boolean[][] result = generator.generate();
-
+  public void toString(boolean[][] result) {
+    PrintStream out = System.out;
     for (int x = 0; x < result.length; x++) {
       for (int y = 0; y < result[x].length; y++) {
         if (result[x][y]) {
-          System.out.print(".");
+          out.print(".");
         } else {
-          System.out.print("#");
+          out.print("#");
         }
       }
-
-      System.out.println();
+      out.println();
     }
+  }
 
+  public static void main(String[] args) {
+    DepthFirstMazeGenerator generator = new DepthFirstMazeGenerator(
+      new MersenneTwister(), 20, 80);
+
+    boolean[][] result = generator.generate();
+
+    generator.toString(result);
   }
 }
